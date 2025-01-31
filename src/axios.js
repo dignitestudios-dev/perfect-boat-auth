3c60b1e1a4e4d2e4d920f8193450d0e126e92d49
steveparker123 <touqeer.hassan@launchbox.pk>
2025-01-31 19:33:53 +0500

    cancell subscription work

--- src/axios.js
+++ src/axios.js
@@ -1,37 +1,39 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const baseUrl = 'https://api.theperfectboat.com';
+export const baseUrl = "https://api.theperfectboat.com";
 
 // export const baseUrl = 'http://92.168.18.141:3000';
 
-
 const instance = axios.create({
-  baseURL: baseUrl, 
+  baseURL: baseUrl,
 });
 
 instance.interceptors.request.use((request) => {
+  // let token = sessionStorage.getItem("token");
+  let token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OWNkOWQ3ZTg2YTFlYzVhNDE4YzExNiIsImlhdCI6MTczODMzMjYzMiwiZXhwIjoxNzQ2MTA4NjMyfQ.0grvYkxQrb9rLBbXCmNe6WhIfWV0HagS46lchPPpy7g";
 
-  let token = sessionStorage.getItem('token')
-  
   request.headers = {
-    'Accept': "application/json, text/plain, */*",
-    'Authorization': `Bearer ${token}`,
-  }
-  return request
+    Accept: "application/json, text/plain, */*",
+    Authorization: `Bearer ${token}`,
+  };
+  return request;
 });
 
-instance.interceptors.response.use((response) => {
-  if (response) {
-    return response;
+instance.interceptors.response.use(
+  (response) => {
+    if (response) {
+      return response;
+    }
+  },
+  function (error) {
+    // *For unAuthorized
+    if (error.response.status === 401) {
+      sessionStorage.clear();
+      // window.location.href="/"
+    }
+    return Promise.reject(error);
   }
-}, function (error) {
-  
-  // *For unAuthorized 
-  if (error.response.status === 401) {
-    sessionStorage.clear()
-    window.location.href="/"
-  }
-  return Promise.reject(error);
-});
+);
 
 export default instance;
--- src/pages/UpdatePlan.jsx
+++ src/pages/UpdatePlan.jsx
@@ -1,63 +1,116 @@
 import axios from "../axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PackageLoader from "../components/PackageLoader";
 import { FiLoader } from "react-icons/fi";
 import { GiStripedSun } from "react-icons/gi";
 import { FaStripeS } from "react-icons/fa6";
+import { SuccessToast } from "../components/Toaster";
 
 const UpdatePlan = () => {
-    const navigate = useNavigate();
-    const [loading, setLoading] = useState(false);
-    const [loadingBtn, setLoadingBtn] = useState([]);
-    const [subscriptions, setSubscriptions] = useState([]);
-    console.log("🚀 ~ UpdatePlan ~ subscriptions:", subscriptions)
-    const [selected, setSelected] = useState([true,false])
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [loadingBtn, setLoadingBtn] = useState([]);
+  const [cancelLoading, setCancelLoading] = useState(false);
+  const [subscriptions, setSubscriptions] = useState([]);
+  const [reasonError, setReasonError] = useState(null);
 
-    const getNotifications = async () => {
-      try {
-        setLoading(true);
-        const { data } = await axios.get("/owner/subscription/plan");
-        setSubscriptions(data?.data);
-      } catch (error) {
-        console.log("Error:", error);
-      } finally {
-        setLoading(false);
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const [selectedReason, setSelectedReason] = useState("");
+  const dropdownRef = useRef(null);
+
+  const reasons = [
+    { reason: "Schedule tasks and get notifications when tasks are due." },
+    { reason: "Complete tasks and add note and photos." },
+    { reason: "Keep and download complete preventive history." },
+    { reason: "Manage up to 3 boats / personal watercraft." },
+  ];
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownVisible(false);
       }
     };
 
-    useEffect(() => {
-      getNotifications();
-    }, []);
+    // document.addEventListener("mousedown", handleClickOutside);
+    // return () => {
+    //   document.removeEventListener("mousedown", handleClickOutside);
+    // };
+  }, []);
 
-    const handlePlan = async (id,index) => {
-      try {
-        setLoadingBtn((prev) => {
-            const newLoadingState = [...prev];
-            newLoadingState[index] = true;
-            return newLoadingState;
-          });
-        const response = await axios.post("/owner/subscription/selectPlan", {
-          subscriptionPlanId: id,
-        });
+  const handleReasonSelect = (reason) => {
+    setSelectedReason(reason);
+    setReasonError(null);
+  };
 
-        if (response.status === 200) {
-          navigate("/summary");
-          setLoadingBtn(false);
-        } else {
-          setError("Payment failed. Please try again.");
-        }
-      } catch (apiError) {
-        setError("An error occurred while processing the payment.");
-        console.error(apiError);
-      } finally {
-        setLoadingBtn((prev) => {
-            const newLoadingState = [...prev];
-            newLoadingState[index] = true;
-            return newLoadingState;
-          });
+  const getNotifications = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get("/owner/subscription/plan");
+      setSubscriptions(data?.data);
+    } catch (error) {
+      console.log("Error:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getNotifications();
+  }, []);
+
+  const handlePlan = async (id, index) => {
+    try {
+      setLoadingBtn((prev) => {
+        const newLoadingState = [...prev];
+        newLoadingState[index] = true;
+        return newLoadingState;
+      });
+      const response = await axios.post("/owner/subscription/selectPlan", {
+        subscriptionPlanId: id,
+      });
+
+      if (response.status === 200) {
+        navigate("/summary");
+        setLoadingBtn(false);
+      } else {
+        setError("Payment failed. Please try again.");
       }
-    };
+    } catch (apiError) {
+      setError("An error occurred while processing the payment.");
+      console.error(apiError);
+    } finally {
+      setLoadingBtn((prev) => {
+        const newLoadingState = [...prev];
+        newLoadingState[index] = true;
+        return newLoadingState;
+      });
+    }
+  };
+
+  const handleCancelSubscription = async () => {
+    if (!selectedReason) {
+      setReasonError("Select cancellation Reason");
+      return;
+    }
+    try {
+      setCancelLoading(true);
+      const response = await axios.put("/owner/subscription/cancel", {
+        reason: selectedReason,
+      });
+
+      if (response.status === 200) {
+        SuccessToast("Subscription Cancel");
+        navigate("/");
+      }
+    } catch (error) {
+      console.log("🚀 ~ handleCancelSubscription ~ error:", error);
+      cancelLoading(false);
+    } finally {
+      cancelLoading(false);
+    }
+  };
   return (
     <div className="w-screen min-h-screen bg-[#001229] flex flex-col gap-4 py-6 px-4 xl:py-0 xl:px-0 items-center justify-center">
       {/* Centered Content */}
@@ -71,13 +124,15 @@ const UpdatePlan = () => {
           tailored experience just for you.
         </p>
         <div>
-         <button onClick={() => navigate("/add-card")}
-         className={`outline-none bg-[#329b53] hover:bg-[#39a265] mt-4
+          <button
+            onClick={() => navigate("/add-card")}
+            className={`outline-none bg-[#329b53] hover:bg-[#39a265] mt-4
              text-white rounded-2xl w-[150px] md:w-[170px] xl:w-[200px] h-[38px]
-             md:h-[40px] xl:h-[44px] flex items-center justify-center gap-2 font-[550]`}>
-            <FaStripeS className="text-white text-xl" /> 
+             md:h-[40px] xl:h-[44px] flex items-center justify-center gap-2 font-[550]`}
+          >
+            <FaStripeS className="text-white text-xl" />
             <span className="text-sm md:text-base">Update Card</span>
-        </button>
+          </button>
         </div>
 
         {loading ? (
@@ -90,6 +145,67 @@ const UpdatePlan = () => {
                 className="w-auto md:w-[420px] xl:w-[440px] h-auto rounded-[20px] xl:rounded-[24px] bg-[#1A293D]
                p-6 md:p-8 flex flex-col gap-6 justify-center items-center"
               >
+                <div className="w-full position relative">
+                  {subscription?.currentPlan && (
+                    <div className="w-full flex justify-end">
+                      <button
+                        className="text-red-600 text-xs"
+                        onClick={() => setDropdownVisible((prev) => !prev)} // Toggle dropdown visibility
+                      >
+                        Cancel Subscription
+                      </button>
+                    </div>
+                  )}
+
+                  {dropdownVisible && (
+                    <div
+                      ref={dropdownRef}
+                      className="absolute mt-2 p-4 w-72 bg-[#2c3f57] border-[#1A293D] rounded-xl shadow text-[#fff]/[0.5] z-10"
+                    >
+                      <div className="w-full text-center">
+                        <p className="text-xl text-white">
+                          Select cancellation reason
+                        </p>
+                      </div>
+
+                      <span>
+                        {reasons.map((item, index) => (
+                          <div
+                            key={index}
+                            className="flex items-center p-2 cursor-pointer "
+                          >
+                            <input
+                              type="radio"
+                              id={`reason-${index}`}
+                              name="reason"
+                              value={item.reason}
+                              // checked={selectedReason === item.reason}
+                              onChange={() => handleReasonSelect(item.reason)}
+                              className="mr-2"
+                            />
+                            <label
+                              htmlFor={`reason-${index}`}
+                              className="cursor-pointer"
+                            >
+                              {item.reason}
+                            </label>
+                          </div>
+                        ))}
+                      </span>
+                      <button
+                        onClick={handleCancelSubscription}
+                        className="mt-2 w-full bg-red-600 text-white p-2 rounded"
+                      >
+                        {cancelLoading ? "Cancelling..." : "Cancel"}
+                      </button>
+                      {reasonError && (
+                        <p className="text-xs font-bold text-red-600">
+                          {reasonError}
+                        </p>
+                      )}
+                    </div>
+                  )}
+                </div>
                 <span
                   className="w-auto h-[36px] md:h-[42px] bg-[#001229] text-center text-[12px] md:text-[14px]
                  tracking-[3px] font-semibold px-3 md:px-4 text-white rounded-full flex items-center justify-center"
@@ -120,7 +236,7 @@ const UpdatePlan = () => {
 
                 <button
                   disabled={subscription?.currentPlan}
-                  onClick={() => handlePlan(subscription?._id,index)}
+                  onClick={() => handlePlan(subscription?._id, index)}
                   className={`outline-none ${
                     subscription?.currentPlan ? "bg-[#1c4353]" : "bg-[#55C9FA]"
                   }  text-white rounded-full w-[100px] md:w-[110px] xl:w-[126px] h-[38px]
@@ -149,6 +265,6 @@ const UpdatePlan = () => {
       </div>
     </div>
   );
-}
+};
 
-export default UpdatePlan
+export default UpdatePlan;
